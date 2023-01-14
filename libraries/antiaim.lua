bd94e85e8cda098cbd17c0016b8a0ce64b32d2e4
Goennigoegoe <79897009+Goennigoegoe@users.noreply.github.com>
2023-01-14 11:35:57 +0100

    Create antiaim.lua

---
+++ libraries/antiaim.lua
@@ -0,0 +1,24 @@
+function Alive(Player)
+    if Player and Player.Character and Player.Character:FindFirstChild("HumanoidRootPart") ~= nil and Player.Character:FindFirstChild("Humanoid") ~= nil and Player.Character:FindFirstChild("Head") ~= nil then
+        return true
+    end
+    return false
+end
+
+local library = {};
+library.__index = library;
+
+function library.Spin(Speed, player)
+    if Alive(player) then
+        player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * CFrame.Angles(0, math.rad(Speed), 0)
+    end
+end
+
+function library.Jitter(Speed, Angle, player)
+    if Alive(player) then
+        local Jit = Speed
+        player.Character.HumanoidRootPart.CFrame = CFrame.new(player.Character.HumanoidRootPart.CFrame.Position) * CFrame.Angles(0, math.rad(Angle) + math.rad((math.random(1, 2) == 1 and Jit or -Jit)), 0)
+    end
+end
+
+return setmetatable({}, library);
