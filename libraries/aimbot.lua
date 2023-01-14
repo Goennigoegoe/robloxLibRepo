744a4739b3b6f58e2cf51ed155e5341c60438b11
Goennigoegoe <79897009+Goennigoegoe@users.noreply.github.com>
2023-01-14 22:01:44 +0100

    Update aimbot.lua

--- libraries/aimbot.lua
+++ libraries/aimbot.lua
@@ -29,23 +29,28 @@ function createTracer(pos1, pos2, camera)
     tracer.Thickness = 1;
     tracer.From = Vector2.new(0, 0);
     tracer.To = Vector2.new(0, 0);
-    tracer.Visible = false
+    tracer.Visible = false;
+    
+    local destroyed = false;
 
     function dotracer()
         game:GetService("RunService").RenderStepped:Connect(function()
-            local vector1, onScreen1 = camera:WorldToViewportPoint(pos1);
-            local from = Vector2.new(vector1.X, vector1.Y);
-            local vector2, onScreen2 = camera:WorldToViewportPoint(pos2);
-            local to = Vector2.new(vector2.X, vector2.Y);
-            if onScreen1 and onScreen2 then
-                tracer.From = from;
-                tracer.To = to;
-                tracer.Visible = true;
-            else
-                tracer.Visible = false;
+            if not destroyed then
+                local vector1, onScreen1 = camera:WorldToViewportPoint(pos1);
+                local from = Vector2.new(vector1.X, vector1.Y);
+                local vector2, onScreen2 = camera:WorldToViewportPoint(pos2);
+                local to = Vector2.new(vector2.X, vector2.Y);
+                if onScreen1 and onScreen2 then
+                    tracer.From = from;
+                    tracer.To = to;
+                    tracer.Visible = true;
+                else
+                    tracer.Visible = false;
+                end
             end
         end)
         task.wait(1)
+        destroyed = true;
         tracer:Remove();
     end
     dotracer();
