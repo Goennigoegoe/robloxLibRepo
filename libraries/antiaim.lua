1c5bfcbbd1a225cdc3f2e02bab4a18f1aacc8311
Goennigoegoe <79897009+Goennigoegoe@users.noreply.github.com>
2023-01-14 12:10:54 +0100

    Update antiaim.lua

--- libraries/antiaim.lua
+++ libraries/antiaim.lua
@@ -23,7 +23,7 @@ end
 
 function library.Angle(Angle, player)
     if Alive(player) then
-        player.Character.HumanoidRootPart.CFrame = CFrame.new(player.Character.HumanoidRootPart.CFrame.Position) * CFrame.Angles(0, math.rad(Angle), 0);
+        player.Character.HumanoidRootPart.CFrame = CFrame.new(player.Character.HumanoidRootPart.CFrame.Position) * CFrame.Angles(0, Angle, 0);
     end
 end
 
